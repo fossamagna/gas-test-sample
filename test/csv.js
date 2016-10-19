84bd6977fa4c1540811b501bac0a73bdff3981bc
MURAKAMI Masahiko <fossamagna2@gmail.com>
2016-10-19 19:27:48 +0900

    Change description for test method.

--- test/csv.js
+++ test/csv.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var csv = require('../src/csv');
 
-suite('csv test', function() {
-  test('csv', function() {
+suite('csv', function() {
+  test('create CSV data from first sheet of spread.', function() {
     var id = '1iT0mv5Iw1QOB4i8hyT2hNJW0Y2kcfQd5Ce9dGn8cz24';
     var result = csv(id);
     assert.equal(result, 'column1,column2,column3\nvalue1_1,value1_2,value1_3\nvalue2_1,value2_2,value2_3');
